5df66ea80c5c983e992a1650a58fd24baa529815
Diego <dhiego1510@gmail.com>
2024-03-23 00:09:16 -0500

    update portfolio

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -16,22 +16,30 @@
     {
       id: 1,
       title: "App Info País",
-      desc: "Aplicacion API REST. Utiliza MongoDB como base de datos para almacenar y gestionar los datos de manera eficiente y escalable",
+      desc: "Aplicacion API REST con Next.js. Utiliza MongoDB como base de datos para almacenar y gestionar los datos de manera eficiente y escalable, usa React para la interfaz de usuario y Express.js para el servidor. ",
       img: "/countries.png",
       link: "https://github.com/JavierRuizArango/code_minds",
       skills: "Next.js, React, Node.js, Express, MongoDB, GraphQL ",
     },
     {
       id: 2,
+      title: "Follow Card Twitter",
+      desc: "Este proyecto es un clon de tarjetas de seguimiento de Twitter, donde se muestran usuarios con la opción de seguirlos o dejar de seguirlos. Cada tarjeta incluye el nombre, nombre de usuario y su avatar obtenido de la API de Unavatar.",
+      img: "/card.png",
+      link: "https://twitter-follow-card-tan.vercel.app/",
+      skills: "React, Vite.js, CSS ",
+    },
+    {
+      id: 3,
       title: "Tarjeta con React",
       desc: "Ésta es mi tarjeta de presentación personal, una página web sencilla que incluye mi foto, información personal y enlaces a mis redes sociales. ",
       img: "/card-bussines.png",
       link: "https://dhiego1510.github.io/project-business-card/",
-      skills: "Vite.js, React ",
+      skills: "Vite.js, React, CSS ",
     },
      
     {
-      id: 3,
+      id: 4,
       title: "Todo-List ",
       desc: "To-Do list es una aplicación web que te permite crear una lista de tareas,añadir por categoría y marcarlas como completadas.",
       img: "/todo-list.png",
@@ -39,13 +47,14 @@
       skills: "Html, Css, Javascript",
     },
     {
-      id: 4,
+      id: 5,
       title: "Barberia website ",
       desc: "Barberia website es una página web sencilla con html y css puro, que incluye sección home, una sección de productos, un formulario de contacto.",
       img: "/barber.png",
       link: "https://barbershop-website-three.vercel.app/",
       skills: "Html, Css",
     },
+     
      
   ];
 
